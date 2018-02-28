fac47c8d476c1dde0cd777e6ca3a36e86b1f524a
Valerie Siira <vlsiira@vt.edu>
2018-02-27 20:46:55 -0800

    click console logs imgName.jpg

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// TODO
+// - get click event to console log which img was clicked
+//    - use event.target.tagName?
+
+function registerImageClick() {
+    if (event.target.tagName === 'IMG') {
+        const index = event.target.src.lastIndexOf('/');
+        console.log(event.target.src.substring(index + 1));
+    }
+}
+
+const elImages = document.getElementById('images');
+elImages.addEventListener('click', registerImageClick);
+
+
+// function addListeners() {
+//     document.getElementById("pictures").addEventListener("click", registerImageClick);
+//     console.log('listening');
+// }
+
+// function registerImageClick(event) {
+
+// }
+
+// window.addEventListener("load", addListeners);
