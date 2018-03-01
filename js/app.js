ba80c2f7a5255141f107f2c64b1e5650e386b536
Valerie Siira <vlsiira@vt.edu>
2018-02-28 16:14:16 -0800

    constructor function added, showProducts & render functions show all images

--- js/app.js
+++ js/app.js
@@ -1,8 +1,54 @@
 'use strict';
 
 // TODO
-// - get click event to console log which img was clicked
-//    - use event.target.tagName?
+// - show images dynamically
+//    - show all through render function, remove from html
+
+function Product (name, filepath) {
+    this.name = name;
+    this.filepath = filepath;
+}
+
+Product.prototype.render = function () {
+    const elImages = document.createElement('img');
+    elImages.src = this.filepath;
+    return elImages;
+}
+
+const analysis = {
+    products: [],
+    selectedProducts: [],
+    start: function () {
+        this.products.push(
+            new Product ('bag', 'img/bag.jpg'),
+            new Product('banana', 'img/banana.jpg'),
+            new Product('boots', 'img/boots.jpg'),
+            new Product('chair', 'img/chair.jpg'),
+            new Product('cthulhu', 'img/cthulhu.jpg'),
+            new Product('dragon', 'img/dragon.jpg'),
+            new Product('pen', 'img/pen.jpg'),
+            new Product('scissors', 'img/scissors.jpg'),
+            new Product('shark', 'img/shark.jpg'),
+            new Product('sweep', 'img/sweep.jpg'),
+            new Product('unicorn', 'img/unicorn.jpg'),
+            new Product('usb', 'img/usb.jpg'),
+            new Product('water-can', 'img/water_can.jpg'),
+            new Product('wine-glass', 'img/wine_glass.jpg'),
+        );
+        
+        this.showProducts();
+        
+        const elContainer = document.getElementById('images-container');
+        elContainer.addEventListener('click', registerImageClick);
+    },
+
+    showProducts: function () {
+        const elContainer = document.getElementById('images-container');
+        for (let i = 0; i < this.products.length; i++) {
+            elContainer.appendChild(this.products[i].render());
+        }
+    }
+}
 
 function registerImageClick() {
     if (event.target.tagName === 'IMG') {
@@ -11,8 +57,7 @@ function registerImageClick() {
     }
 }
 
-const elImages = document.getElementById('images');
-elImages.addEventListener('click', registerImageClick);
+analysis.start();
 
 
 // function addListeners() {
